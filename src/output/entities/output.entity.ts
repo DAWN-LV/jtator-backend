64edd61349a5168d465d516a8d0dfd96d773bedb
Vitalijs Pankovs <vitalijs.pankovs@gmail.com>
2024-05-12 17:14:56 +0300

    Added new outputs.

---
+++ src/output/entities/output.entity.ts
@@ -0,0 +1,136 @@
+export default [
+  {
+    "type": "paper",
+    "info": {
+      "type": "a5",
+      "pages": 1,
+      "name": "Paper Sheet"
+    },
+    "units": {
+      "paper": 0.0025,
+    }
+  },
+  {
+    "type": "paper",
+    "info": {
+      "type": "a4",
+      "pages": 1,
+      "name": "Paper Sheet"
+    },
+    "units": {
+      "paper": 0.005,
+    }
+  },
+  {
+    "type": "paper",
+    "info": {
+      "type": "a3",
+      "pages": 1,
+      "name": "Paper Sheet"
+    },
+    "units": {
+      "paper": 0.01,
+    }
+  },
+  {
+    "type": "degree",
+    "info": {
+      "type": "a4",
+      "pages": 1,
+      "name": "Degree"
+    },
+    "units": {
+      "paper": 0.005,
+      "ink": 0.02,
+    }
+  },
+  {
+    "type": "notebook",
+    "info": {
+      "type": "a5",
+      "pages": 80,
+      "name": "Notebook"
+    },
+    "units": {
+      "paper": 0.2,
+      "glue": 0.05,
+      "ink": 0.01
+    }
+  },
+  {
+    "type": "notebook",
+    "info": {
+      "type": "a4",
+      "pages": 100,
+      "name": "Notebook"
+    },
+    "units": {
+      "paper": 0.4,
+      "glue": 0.07,
+      "ink": 0.02
+    }
+  },
+  {
+    "type": "sketchbook",
+    "info": {
+      "type": "a4",
+      "pages": 50,
+      "name": "Sketchbook"
+    },
+    "units": {
+      "paper": 0.25,
+      "glue": 0.03
+    }
+  },
+  {
+    "type": "diary",
+    "info": {
+      "type": "a5",
+      "pages": 120,
+      "name": "Diary"
+    },
+    "units": {
+      "paper": 0.36,
+      "glue": 0.08,
+      "ink": 0.03
+    }
+  },
+  {
+    "type": "journal",
+    "info": {
+      "type": "a5",
+      "pages": 150,
+      "name": "Journal"
+    },
+    "units": {
+      "paper": 0.3,
+      "glue": 0.06,
+      "ink": 0.025
+    }
+  },
+  {
+    "type": "planner",
+    "info": {
+      "type": "a6",
+      "pages": 200,
+      "name": "Planner"
+    },
+    "units": {
+      "paper": 0.32,
+      "glue": 0.09,
+      "ink": 0.04
+    }
+  },
+  {
+    "type": "photo_album",
+    "info": {
+      "type": "a4",
+      "pages": 30,
+      "name": "Photo Album"
+    },
+    "units": {
+      "paper": 0.15,
+      "glue": 0.05
+    }
+  }
+]
